78f455100a1ea541485ac5d5e7311670c9261afc
kokon322 <chornuy322@gmail.com>
2020-07-08 22:15:31 +0200

    finish next task in task1 and coment it

--- main/lesson3_DOM_objects_divide/task1/main.js
+++ main/lesson3_DOM_objects_divide/task1/main.js
@@ -1,4 +1,10 @@
 /* // створити об'єкт (не меньше 5ти властивостей) який описує
+- собаку
+- людину
+- автомобіль
+- квартиру
+- книгу
+
 let obj = {
     
     dog :{name : "baddy",
@@ -32,3 +38,24 @@ let obj = {
                         year: 1973}
     
 }; */
+/* //-- Створити масив та вивести його в консоль:
+//- з 5 собак
+//- 3 5 людей
+//- з 5 автомобілів
+let mas = [
+    dog="Bady",
+        dog1 = "Gera",
+            dog2 = "Archi",
+                dog3 = "Kima",
+                    dog4 = "Jora",
+                human = "Vasa",
+                    human1 = "Olga",
+                        human2 = "Lila",
+                            human3 = "Ivan",
+                                human4 = "Petro",
+                car = "Subaru",
+                    car1 = "Toyota",
+                        car2 = "Honda",
+                            car3 = "Lexus",
+                                car4 = "BMW",];
+console.log(mas); */
