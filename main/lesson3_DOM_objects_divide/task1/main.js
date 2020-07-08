b28b9e858ed9c7c89929c8934364de7837aa629f
kokon322 <chornuy322@gmail.com>
2020-07-08 22:01:35 +0200

    first change in task 1 and comet him

---
+++ main/lesson3_DOM_objects_divide/task1/main.js
@@ -0,0 +1,34 @@
+// створити об'єкт (не меньше 5ти властивостей) який описує
+let obj = {
+    
+    dog :{name : "baddy",
+     age: 12,
+      color: "orange",
+       breed: "staf",
+        cities: "Vinnitsa"},
+    human :{
+        name: "Igor",
+            age: 28,
+                color: "Wight",
+                    wife: true,
+                        child: true},
+    car:{
+        firstName: "subaru",
+            secondName: "outback",
+                year: 2017,
+                    engineLitters: 2.5,
+                        orCarMy: true},
+    flat:{
+        flatInCity: "Vinnitsa",
+            floor: 4,
+                lift: true,
+                    rooms: 2,
+                        window: 3},
+    book:{
+        favorite: "Тереодори з Васюкывки",
+            length: "Українська",
+                paper: true,
+                    author:"Всеволод нестайко",
+                        year: 1973}
+    
+};
