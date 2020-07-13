6a4df6d21ff02bb1b7222e4eea2b17ebdcff4b8b
kokon322 <chornuy322@gmail.com>
2020-07-13 16:28:44 +0200

    coment it

--- main/lesson4_divide/main.js
+++ main/lesson4_divide/main.js
@@ -1,11 +1,11 @@
 // створити функцію яка виводить масив
 // - створити функцію яка заповнює масив рандомними числами та виводить його. Для виведення використати попвередню функцію.
-function masRandom(){
-    let array = [];
-    for(let i = 0;i<20;i++){
-        let x = Math.floor(Math.random()*100);
-        array[i] = x;
-    }
-    console.log(array);
-}
-masRandom();
+// function masRandom(){
+//     let array = [];
+//     for(let i = 0;i<20;i++){
+//         let x = Math.floor(Math.random()*100);
+//         array[i] = x;
+//     }
+//     console.log(array);
+// }
+// masRandom();
