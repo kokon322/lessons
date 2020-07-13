c382ff73d2387f8a4890921f751dc9dfbfadddf5
kokon322 <chornuy322@gmail.com>
2020-07-13 20:49:13 +0200

    finish some task and coment it

--- main/lesson4_divide/main.js
+++ main/lesson4_divide/main.js
@@ -32,3 +32,37 @@
 // console.log(max(320,140,25));
 // створити функцію яка приймає будь-яку кількість чисел,
 // повертає найменьше, а виводить найбільше
+// function maxMin(){
+//     let max =arguments[0];
+//     let min =arguments[0];
+//     for(let i =0;i<arguments.length;i++){
+//         if (arguments[i]>max){
+//             max = arguments[i];
+            
+//         }if (arguments[i]<min){
+//             min = arguments[i];
+//         }
+//     }document.write(max, "<br>" );
+//     return min;
+    
+// }
+
+// console.log(maxMin(4,3,12,0,558,94,1,-1))
+//// Так як і раніше почергово коментуємо)))
+//створити функцію яка виводить масив
+// function mas(){
+//     let array = [];
+//     console.log(array);
+// }
+// mas();
+//створити функцію яка повертає найбільше число з масиву
+// function Bigest (mas = []){
+//     let Bigmas = mas[0];
+//     for(let i =0;i<mas.length;i++){
+//         if(Bigmas < mas[i]){
+//             Bigmas = mas[i];
+//         }
+//     }return Bigmas;
+// }
+// console.log(Bigest([36,12,25,,12,,12,9]))
+//створити функцію яка повертає найменьше число з масиву
