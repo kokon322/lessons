5a886595683efb09ed2ee31538c1bc5610998fa2
kokon322 <chornuy322@gmail.com>
2020-07-14 19:21:17 +0200

    try to finish task...

--- main/lesson4_divide/main.js
+++ main/lesson4_divide/main.js
@@ -202,4 +202,34 @@
 // }
 // addTagText("Hard...", "p");
 //Завдання з автомобілями мене просто поставило на коліна... тому його пропустив.
+// функція приймає 2 масиви з рівною кількістю об'єктів 
+// та з'єднює в один об'єкт користувача та місто з відповідними "id"
+//  та "user_id",
+// та повертає масив цих з'єднаних об'єктів.
+// з цим теж кака...
+// let usersWithId = [
+//     {id: 1, name: 'vasya', age: 31, status: false},
+//     {id: 2, name: 'petya', age: 30, status: true},
+//     {id: 3, name: 'kolya', age: 29, status: true},
+//     {id: 4, name: 'olya', age: 28, status: false},
+// ];
 
+// let citiesWithId = [
+//     {user_id: 3, country: 'USA', city: 'Portland'},
+//     {user_id: 1, country: 'Ukraine', city: 'Ternopil'},
+//     {user_id: 2, country: 'Poland', city: 'Krakow'},
+//     {user_id: 4, country: 'USA', city: 'Miami'},
+// ];
+
+
+// function addCity(array, array1) {
+//     let masResult = [];
+//     for(let i =0;i<array;i++){
+//         for (let j =0;j<array1;j++){
+//             if(array[i].id === array1[j].user_id){
+//                masResult.push(array[i],array1[j])
+//             }
+//         }
+//     }return masResult;
+// }
+// console.log(addCity(usersWithId,citiesWithId))
