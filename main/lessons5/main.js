3ab3a29ebd3eb16861c155df585ca201add8aec8
kokon322 <chornuy322@gmail.com>
2020-07-15 11:34:08 +0200

    add class Tags

--- main/lessons5/main.js
+++ main/lessons5/main.js
@@ -61,3 +61,31 @@ class Tags{
     }
 }
 
+let a = new Tags('a','В зависимости от присутствия атрибутов name или href тег <a> устанавливает ссылку или якорь',
+[{nameAtributs: 'href', functionAtribut: 'Задает адрес документа, на который следует перейти.'},
+{nameAtributs: 'name', functionAtribut: 'Устанавливает имя якоря внутри документа'}]);
+
+let div = new Tags('div','Элемент <div>предназначен для выделения фрагмента документа с целью изменения вида содержимого',
+[{nameAtributs: 'align', functionAtribut: 'Задает выравнивание содержимого тега <div>.'},
+{nameAtributs: 'title', functionAtribut: 'Добавляет всплывающую подсказку к содержимому.'}]);
+
+let h1 = new Tags('h1','тег <h1> представляет собой наиболее важный заголовок первого уровня',
+[{nameAtributs: 'align',functionAtribut: 'Определяет выравнивание заголовка.' }]);
+
+let span = new Tags('span', 'Тег <span> предназначен для определения строчных элементов документа',
+[{nameAtributs:'class',functionAtribut:'Определяет имя класса, которое позволяет связать тег со стилевым оформлением.' },
+{nameAtributs: 'dir', functionAtribut: 'Задает направление и отображение текста — слева направо или справа налево.'}]);
+
+let input = new Tags('input','позволяет создавать разные элементы интерфейса и обеспечить взаимодействие с пользователем',
+[{nameAtributs:'checked',functionAtribut:'Предварительно активированный переключатель или флажок.'},{nameAtributs:'list',functionAtribut:'Указывает на список вариантов, которые можно выбирать при вводе текста.'}]);
+
+let form = new Tags('form','устанавливает форму на веб-странице',
+[{nameAtributs:'target',functionAtribut:'Имя окна или фрейма, куда обработчик будет загружать возвращаемый результат.'},
+{nameAtributs:'method',functionAtribut:'Метод протокола HTTP.'}]);
+
+let option = new Tags('option', 'определяет отдельные пункты списка, создаваемого с помощью контейнера <select>',
+[{nameAtributs:'disabled',functionAtribut:'Заблокировать для доступа элемент списка.'},{nameAtributs:'label',functionAtribut:'Указание метки пункта списка.'}]);
+
+let select = new Tags('select','позволяет создать элемент интерфейса в виде раскрывающегося списка',
+[{nameAtributs:'size',functionAtribut:'Количество отображаемых строк списка.'},{nameAtributs:'multiple',functionAtribut:'Позволяет одновременно выбирать сразу несколько элементов списка.'}]);
+
