ca79086d1ff1d6f4c258819703ca724021924c0d
kokon322 <chornuy322@gmail.com>
2020-07-17 17:29:49 +0200

    finish some tasc and coment it

---
+++ main/Lesson6/main.js
@@ -0,0 +1,20 @@
+//- создать массив с 20 числами.
+//let array = [9, 45, 8, 19, 108, 71, 67, 18, 115, -6, 29, 25, 70, 73, 15, 11, 106, 115, 35, 97];
+//-- при помощи метода sort и колбека  отсортировать массив.
+// array.sort((a,b)=> a-b);
+// console.log(array);
+//-- при помощи метода sort и колбека отсортировать массив в ниспадающем напралении.
+// array.sort((a,b)=>b-a)
+// console.log(array);
+//-- при помощи filter получить числа кратные 3
+// let three = array.filter(value => !(value%3));
+// console.log(three) 
+//-- при помощи filter получить числа кратные 10
+// let ten = array.filter(value => !(value%10))
+// console.log(ten);
+//-- перебрать (проитерировать) массив при помощи foreach()
+//array.forEach((value) => console.log(value));
+//-- перебрать массив при помощи map() и получить новый массив в котором все
+// значения будут в 3 раза больше
+// let newMas = array.map((value)=> value*3);
+// console.log(newMas);
