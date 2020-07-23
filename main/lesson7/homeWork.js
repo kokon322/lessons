bab78dca16463217726cc0bb6d3c521e05a5cb0f
kokon322 <chornuy322@gmail.com>
2020-07-23 18:57:17 +0200

    done homeWork

--- main/lesson7/homeWork.js
+++ main/lesson7/homeWork.js
@@ -225,7 +225,7 @@
 // }
 // divText.appendChild(ul);
 
-// divText.style.widthwidth = '30%';
+// divText.style.widthwidth = '100%';
 // wrap.style.width = '70%';
 // divText.style.float = 'left';
 // wrap.style.float = 'left';
@@ -233,18 +233,19 @@
 
 // -- взять массив пользователей
 // let usersWithAddress = [
-//                 {id:1,name: 'vasya', age: 31, status: false, address: {city: 'Lviv', street: 'Shevchenko', number: 16}},
-//                 {id:2,name: 'petya', age: 30, status: true, address: {city: 'Kyiv', street: 'Shevchenko', number: 1}},
-//                 {id:3,name: 'kolya', age: 29, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 121}},
-//                 {id:4,name: 'olya', age: 28, status: false, address: {city: 'Ternopil', street: 'Shevchenko', number: 90}},
-//                 {id:5,name: 'max', age: 30, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 115}},
-//                 {id:6,name: 'anya', age: 31, status: false, address: {city: 'Kyiv', street: 'Shevchenko', number: 2}},
-//                 {id:7,name: 'oleg', age: 28, status: false, address: {city: 'Ternopil', street: 'Shevchenko', number: 22}},
-//                 {id:8,name: 'andrey', age: 29, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 43}},
-//                 {id:9,name: 'masha', age: 30, status: true, address: {city: 'Kyiv', street: 'Shevchenko', number: 12}},
-//                 {id:10,name: 'olya', age: 31, status: false, address: {city: 'Lviv', street: 'Shevchenko', number: 16}},
-//                 {id:11,name: 'max', age: 31, status: true, address: {city: 'Ternopil', street: 'Shevchenko', number: 121}}
+//  {id:1,name: 'vasya', age: 31, status: false, address: {city: 'Lviv', street: 'Shevchenko', number: 16}},
+//  {id:2,name: 'petya', age: 30, status: true, address: {city: 'Kyiv', street: 'Shevchenko', number: 1}},
+//  {id:3,name: 'kolya', age: 29, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 121}},
+//  {id:4,name: 'olya', age: 28, status: false, address: {city: 'Ternopil', street: 'Shevchenko', number: 90}},
+//  {id:5,name: 'max', age: 30, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 115}},
+//  {id:6,name: 'anya', age: 31, status: false, address: {city: 'Kyiv', street: 'Shevchenko', number: 2}},
+//  {id:7,name: 'oleg', age: 28, status: false, address: {city: 'Ternopil', street: 'Shevchenko', number: 22}},
+//  {id:8,name: 'andrey', age: 29, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 43}},
+//  {id:9,name: 'masha', age: 30, status: true, address: {city: 'Kyiv', street: 'Shevchenko', number: 12}},
+//  {id:10,name: 'olya', age: 31, status: false, address: {city: 'Lviv', street: 'Shevchenko', number: 16}},
+//  {id:11,name: 'max', age: 31, status: true, address: {city: 'Ternopil', street: 'Shevchenko', number: 121}}
 //             ];
+
 // Создать три чекбокса. 
 // Каждый из них активирует фильтр для вышеуказаного массива. 
 // Фильтры могут работать как вместе так и по отдельности.
@@ -253,6 +254,64 @@
 // 2й - оставляет старше 29 лет включительно
 // 3й - оставляет тех у кого город киев
 // Данные выводить в документ
+
+// let divText = document.getElementById('text');
+
+// let inpyt1 = document.createElement('input');
+// let inpyt2 = document.createElement('input');
+// let inpyt3 = document.createElement('input');
+
+// let button = document.createElement('button');
+
+// let label1 = document.createElement('label');
+// let label2 = document.createElement('label');
+// let label3 = document.createElement('label');
+
+// button.innerText = 'filter';
+// inpyt1.type = 'checkbox';
+// inpyt2.type = 'checkbox';
+// inpyt3.type = 'checkbox';
+
+// label1.innerText = 'со статусом false';
+// label2.innerText = 'старше 29 лет ';
+// label3.innerText = 'город киев';
+
+// divText.appendChild(inpyt1);
+// divText.appendChild(label1);
+// divText.appendChild(inpyt2);
+// divText.appendChild(label2);
+// divText.appendChild(inpyt3);
+// divText.appendChild(label3);
+// divText.appendChild(button);
+
+// let masStatus =[];
+// let masAge = [];
+// let masCity = [];
+
+// button.onclick = ()=>{
+//     if(inpyt1.checked){
+//         masStatus = usersWithAddress.filter((value)=>{
+//             if(value.status){
+//                 return false;
+//             }return true;
+//         });
+//     }console.log(masStatus);
+//     if(inpyt2.checked){
+//         masAge = usersWithAddress.filter((value)=>{
+//             if(value.age >= 29){
+//                 return true;
+//             }return false;
+//         });
+//     }console.log(masAge);
+//     if(inpyt3.checked){
+//         masCity = usersWithAddress.filter((value)=>{
+//             if(value.address.city === 'Kyiv'){return true;
+//             }return false;
+//         });
+//     }console.log(masCity);
+//     };
+        
+
 
 
 
